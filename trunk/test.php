1e04d8dd5ea1cd2f2d5c8dea79c6323ec41cf8c2
lacroix.tommy@gmail.com <lacroix.tommy@gmail.com@4160dc10-5686-11de-bbaa-f1a44a2b9d47>
2009-06-11 14:12:31 +0000

    First revision, beta

---
+++ trunk/test.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * MP4Info Test Script
+ * 
+ * @author 		Tommy Lacroix <lacroix.tommy@gmail.com>
+ * @copyright   Copyright (c) 2006-2009 Tommy Lacroix
+ * @license		LGPL version 3, http://www.gnu.org/licenses/lgpl.html
+ * @package 	php-mp4info
+ * @link 		$HeadURL$
+ */
+
+// ---
+
+include "MP4Info.php";
+
+print '<h1>MP4Info test script</h1>';
+print '<p><small>'.__FILE__.' $Id$</small></p>';
+print '<hr />';
+
+$dir = './TestFiles/';
+$de = opendir($dir);
+if ($de) {
+	while (($file = readdir($de)) !== false) {
+		$path = $dir.$file;
+		if ((!is_file($path)) || (!is_readable($path)) || (strtolower(pathinfo($path,PATHINFO_EXTENSION) != 'f4v'))) continue;
+		
+		print '<h2>'.$file.'</h2>';
+		print "<pre>";
+		try {
+			print_r(MP4Info::getInfo($path));
+		} catch (MP4Info_Exception $e) {
+			print 'Caught MP4Info_Exception with message '.$e->getMessage();
+			throw ($e);
+		} catch (Exception $e) {
+			print 'Cauth Exception with message '.$e->getMessage();
+			throw ($e);
+		}
+		print "</pre>";
+		print '<hr/>';
+	}
+} else {
+	print '<strong>Could not open directory "'.$dir.'".';
+}
+
